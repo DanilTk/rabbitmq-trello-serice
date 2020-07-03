b1aaa16a4c6827ef60413de362a1054ffe779017
Danylo Tkachenko <d.y.tkachenko@gmail.com>
2020-07-03 12:36:58 +0200

    Testing

--- src/main/java/pl/dtkachenko/trelloserice/messaging/RabbitMQPublisher.java
+++ src/main/java/pl/dtkachenko/trelloserice/messaging/RabbitMQPublisher.java
@@ -11,9 +11,9 @@ import pl.dtkachenko.trelloserice.model.Task;
 public class RabbitMQPublisher {
     private RabbitTemplate rabbitTemplate;
     @Value("${rabbitmq.routingKey}")
-    public String ROUTING_KEY = "trello-key";
+    private String ROUTING_KEY = "trello-key";
     @Value("${rabbitmq.exchangeName}")
-    public String EXCHANGE_NAME;
+    private String EXCHANGE_NAME;
 
     public RabbitMQPublisher(RabbitTemplate rabbitTemplate) {
         this.rabbitTemplate = rabbitTemplate;
---
+++ src/test/java/pl/dtkachenko/trelloserice/messaging/RabbitMQPublisherTest.java
@@ -0,0 +1,56 @@
+package pl.dtkachenko.trelloserice.messaging;
+
+import org.hamcrest.core.Is;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.system.OutputCaptureRule;
+import org.springframework.boot.test.util.TestPropertyValues;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.testcontainers.containers.GenericContainer;
+import pl.dtkachenko.trelloserice.model.Task;
+
+import java.time.LocalDateTime;
+import java.util.concurrent.Callable;
+import java.util.concurrent.TimeUnit;
+
+import static org.awaitility.Awaitility.await;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@ContextConfiguration(initializers = RabbitMQPublisherTest.Initializer.class)
+class RabbitMQPublisherTest {
+    @ClassRule
+    public static GenericContainer rabbit = new GenericContainer("rabbitmq:3-management").withExposedPorts(5672, 15672);
+    @Rule
+    public OutputCaptureRule outputCapture = new OutputCaptureRule();
+    @Autowired
+    RabbitMQPublisher publisher;
+
+    @Test
+    void publishToQueue() {
+        Task task = new Task("Task", "Description", LocalDateTime.now(), LocalDateTime.now(), LocalDateTime.now());
+        publisher.publishToQueue(task);
+        await().atMost(5, TimeUnit.SECONDS).until(isMessageConsumed(), Is.is(true));
+    }
+
+    private Callable<Boolean> isMessageConsumed() {
+        return () -> outputCapture.toString().contains("Task");
+    }
+
+    public static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+        @Override
+        public void initialize(ConfigurableApplicationContext configurableApplicationContext) {
+            TestPropertyValues values = TestPropertyValues.of(
+                    "spring.rabbitmq.host=" + rabbit.getContainerIpAddress(),
+                    "spring.rabbitmq.port=" + rabbit.getMappedPort(5672));
+            values.applyTo(configurableApplicationContext);
+        }
+    }
+}
